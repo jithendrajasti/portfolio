013950158f7483f779a9e36055104a3a2b24a642
jithendra <jithendrajasti11@gmail.com>
2025-11-06 20:30:14 +0530

    added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
-  
+
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme toggle */}
@@ -14,6 +15,9 @@ const Home = () => {
       {/*  navbar */}
       <Navbar />
       {/* main content */}
+      <main>
+        <HeroSection/>
+      </main>
       {/* footer */}
     </div>
   );
