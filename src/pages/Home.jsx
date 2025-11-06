ff072967c411870d953c6e8d652983f6a94efbf9
jithendra <jithendrajasti11@gmail.com>
2025-11-06 12:45:47 +0530

    theme toggling completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ThemeToggle from "../components/ThemeToggle";
 
 const Home = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme toggle */}
+      <ThemeToggle />
       {/* Background effects */}
       {/*  navbar */}
       {/* main content */}
