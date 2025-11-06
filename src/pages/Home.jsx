29bf8b9c27b51cc47465ef9f1b9871940a099fca
jithendra <jithendrajasti11@gmail.com>
2025-11-06 19:21:46 +0530

    starbackground added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ThemeToggle from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       {/* Theme toggle */}
       <ThemeToggle />
       {/* Background effects */}
+      <StarBackground />
       {/*  navbar */}
       {/* main content */}
       {/* footer */}
